5e360167d6343e62d22ecc54b188f68e5f051669
Aleksei Vambol <77882392+AlekseiVambol@users.noreply.github.com>
2023-08-28 13:13:38 +0300

    Add files via upload

--- src/secp256r1/fq.rs
+++ src/secp256r1/fq.rs
@@ -176,7 +176,9 @@ impl ff::Field for Fq {
 
     /// Returns the multiplicative inverse of the
     /// element. If it is zero, the method fails.
-    fn invert(&self) -> CtOption<Self> { self.invert() }
+    fn invert(&self) -> CtOption<Self> {
+        self.invert()
+    }
 
     fn pow_vartime<S: AsRef<[u64]>>(&self, exp: S) -> Self {
         let mut res = Self::one();
