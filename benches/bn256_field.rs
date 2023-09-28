ff12af98b815f4ac16e383df093c9ef544021993
Aleksei Vambol <77882392+AlekseiVambol@users.noreply.github.com>
2023-09-28 16:08:01 +0300

    Add files via upload

--- benches/bn256_field.rs
+++ benches/bn256_field.rs
@@ -1,8 +1,7 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
-use halo2curves::bn256::*;
-use halo2curves::ff::Field;
-use rand::SeedableRng;
+use halo2curves::{bn256::*, ff::Field, legendre::Legendre};
 use rand_xorshift::XorShiftRng;
+use rand::SeedableRng;
 
 pub fn bench_bn256_field(c: &mut Criterion) {
     let mut rng = XorShiftRng::from_seed([
@@ -42,6 +41,12 @@ pub fn bench_bn256_field(c: &mut Criterion) {
     });
     group.bench_function("bn256_fq_invert", |bencher| {
         bencher.iter(|| black_box(&a).invert())
+    });
+    group.bench_function("bn256_fq_legendre", |bencher| {
+        bencher.iter(|| black_box(&a).legendre())
+    });
+    group.bench_function("bn256_fq_jacobi", |bencher| {
+        bencher.iter(|| black_box(&a).jacobi())
     });
 }
 
