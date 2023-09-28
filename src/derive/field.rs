51c9c08a20d996cd350732f33fdf009367e8fbf6
Aleksei Vambol <77882392+AlekseiVambol@users.noreply.github.com>
2023-09-28 16:38:51 +0300

    Add files via upload

--- src/derive/field.rs
+++ src/derive/field.rs
@@ -359,23 +359,26 @@ macro_rules! field_common {
                 Ok(())
             }
         }
-        
+
         #[test]
         fn test_jacobi() {
             use rand::SeedableRng;
             use $crate::ff::Field;
             use $crate::legendre::Legendre;
             let mut rng = rand_xorshift::XorShiftRng::from_seed([
-                0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06, 0xbc, 0xe5
+                0x59, 0x62, 0xbe, 0x5d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06,
+                0xbc, 0xe5,
             ]);
             for _ in 0..100000 {
                 let e = $field::random(&mut rng);
                 assert_eq!(
+                    e.legendre(),
                     match e.jacobi() {
                         1 => $field::ONE,
                         -1 => -$field::ONE,
-                        _ => $field::ZERO
-                    }, e.legendre());
+                        _ => $field::ZERO,
+                    }
+                );
             }
         }
     };
